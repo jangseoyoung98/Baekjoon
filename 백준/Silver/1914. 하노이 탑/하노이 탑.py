ccce2abeb26b055b5164190682d831283e82b9e2
jangseoyoung98 <77472570+jangseoyoung98@users.noreply.github.com>
2023-04-08 11:11:39 +0900

    Update 하노이 탑.py

--- 백준/Silver/1914. 하노이 탑/하노이 탑.py
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.py
@@ -1,12 +1,12 @@
-def f(n, a, b, c):
-  if(n == 1):
-    print(a, c, sep = " ")
-  else:
-    f(n-1, a, c, b)
-    f(1, a, b, c)
-    f(n-1, b, a, c)
+def hanoi(N, start, end, sub):
+    if N == 1:
+        print(start, end, sep=" ")
+    else:
+        hanoi(N-1, start, sub, end)
+        hanoi(1, start, end, sub)
+        hanoi(N-1, sub, end, start)
 
-n = int(input())
-print(2**n-1)
-if(n <= 20):
-  f(n, 1, 2, 3)
+N = int(input())
+print(2**N - 1)
+if(N <= 20):
+    hanoi(N, 1, 3, 2)
