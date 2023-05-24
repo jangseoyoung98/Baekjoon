c2a41c20256722f88d7c541076f75a50c39079a3
jangseoyoung98 <77472570+jangseoyoung98@users.noreply.github.com>
2023-05-24 15:18:39 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -2,14 +2,18 @@ import sys
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
-coins = [int(input()) for _ in range(N)]
-coins.sort(reverse=True)
-cnt = 0
+vals = []
+for _ in range(N):
+    val = int(input())
+    vals.append(val)
 
-for i in coins:
-    cnt += K // i
-    K %= i
-    if K == 0:
-        break
+rem = K  # remainder
+cnt = 0
+i = N - 1  # val의 큰 값부터 시작한다.
+
+while i>=0 :
+    cnt += rem // vals[i]
+    rem = rem % vals[i]
+    i -= 1
 
 print(cnt)
